0620563358558682c94688c76c46f3bf3ee41d1d
chshouyu <chshouyu@126.com>
2015-07-23 01:08:31 +0800

    add show more support

--- app/js/item.js
+++ app/js/item.js
@@ -1,8 +1,23 @@
 
 var React = require('react');
 
-var Item = React.createClass({
+var cx = React.addons.classSet;
 
+var CONTENT_LENGTH = 36;
+
+var Item = React.createClass({
+    getInitialState () {
+        return {
+            showAll: false
+        };
+    },
+    toggleAll () {
+        this.setState((state) => {
+            return {
+                showAll: !state.showAll
+            };
+        });
+    },
     render () {
 
         var imgNodes = this.props.item.imgList.map((src, index) => {
@@ -13,12 +28,21 @@ var Item = React.createClass({
             );
         });
 
+        var content = this.props.item.content;
+        var showMore = content.length > CONTENT_LENGTH;
+
+        var classes = cx({
+            'more': true,
+            'open': this.state.showAll
+        });
+
         return (
             <li>
-                <p>{ this.props.item.content }</p>
+                <p onClick={ showMore ? this.toggleAll : null }>{ this.state.showAll ? content : content.slice(0, CONTENT_LENGTH) }</p>
                 <div className="img-list">
                     { imgNodes }
                 </div>
+                { showMore ? <a href="javascript:;" className={ classes } onClick={ this.toggleAll }></a> : null }
             </li>
         );
     }
