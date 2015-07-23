10491accc1bb44882e5b54a543986148298f0546
chshouyu <chshouyu@126.com>
2015-07-23 16:08:43 +0800

    complete gallery

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -3,21 +3,54 @@ require('../css/gallery.less');
 
 var React = require('react');
 
+var IScroll = require('./lib/iscroll-probe.js');
+
 var cx = React.addons.classSet;
 
 var windowWidth = window.innerWidth;
+var iscrollInstance = null;
 
 var Gallery = React.createClass({
     getInitialState () {
         return {
-            isShow: false
+            isShow: false,
+            currPage: 0
         };
     },
     componentWillReceiveProps (nextProps) {
+
         if (nextProps.currIndex > -1) {
             this.setState({
-                isShow: true
+                isShow: true,
+                currPage: nextProps.currIndex
+            }, () => {
+                if (!iscrollInstance) {
+                    iscrollInstance = new IScroll(React.findDOMNode(this), {
+                        eventPassthrough: true,
+                        scrollX: true,
+                        scrollY: false,
+                        snap: true,
+                        momentum: false
+                    });
+
+                    iscrollInstance.on('scrollEnd', () => {
+                        this.setState({
+                            currPage: iscrollInstance.currentPage.pageX
+                        });
+                    });
+                }
+
+                iscrollInstance.refresh();
+                
+                iscrollInstance.goToPage(this.props.currIndex, 0, 0);
             });
+        }
+        
+    },
+    componentWillUnmount () {
+        if (iscrollInstance) {
+            iscrollInstance.destroy();
+            iscrollInstance = null;
         }
     },
     hide () {
@@ -47,6 +80,7 @@ var Gallery = React.createClass({
         return (
             <div className={ classes }>
                 <ul style={ listStyle }>{ imgNodes }</ul>
+                <div className="pager">{ this.state.currPage + 1 } / { this.props.imgList.length }</div>
             </div>
         );
     }
